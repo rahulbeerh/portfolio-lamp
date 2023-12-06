503288c3309da2ed4033521fe68c6619328d3f4c
rahulbeerh <129044167+rahulbeerh@users.noreply.github.com>
2023-12-06 16:24:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const btn=document.querySelector('button.menu');
-const btnClose=document.querySelector('.close-container');
-btn.addEventListener('click',function(){
-    const mobileNavigation=document.querySelector(".mobile-navigation");
-    mobileNavigation.style.transform = "translateX(0rem)";
-})
+ const btn = $('button.menu');
+    const btnClose = $('.close-container');
+    const mobileNavigation = $('.mobile-navigation');
 
-btnClose.addEventListener('click',function(){
-    const mobileNavigation=document.querySelector(".mobile-navigation");
-    mobileNavigation.style.transform = "translateX(200rem)";
-})
+    btn.on('click', function(){
+        mobileNavigation.css('transform', 'translateX(0rem)');
+    });
+
+    btnClose.on('click', function(){
+        mobileNavigation.css('transform', 'translateX(200rem)');
+    });
