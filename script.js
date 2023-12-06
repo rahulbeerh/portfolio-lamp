1db65be834a800111eea13ee1d8c757b8615728a
rahulbeerh <129044167+rahulbeerh@users.noreply.github.com>
2023-12-06 12:47:04 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const btn=document.querySelector('button.menu');
-const btnClose=document.querySelector('.close-container');
-btn.addEventListener('click',function(){
-    const mobileNavigation=document.querySelector(".mobile-navigation");
-    mobileNavigation.style.transform = "translateX(0rem)";
-})
+$(document).ready(function(){
+    const btn = $('button.menu');
+    const btnClose = $('.close-container');
+    const mobileNavigation = $('.mobile-navigation');
 
-btnClose.addEventListener('click',function(){
-    const mobileNavigation=document.querySelector(".mobile-navigation");
-    mobileNavigation.style.transform = "translateX(200rem)";
-})
+    btn.on('click', function(){
+        mobileNavigation.css('transform', 'translateX(0rem)');
+    });
+
+    btnClose.on('click', function(){
+        mobileNavigation.css('transform', 'translateX(200rem)');
+    });
+});
